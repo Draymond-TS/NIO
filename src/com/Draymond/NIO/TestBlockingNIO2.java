2feb01baffdd436478b4eda51373ef5b48ec91fe
Draymond-TS <846933717@qq.com>
2019-09-16 17:03:00 +0800

    V4

---
+++ src/com/Draymond/NIO/TestBlockingNIO2.java
@@ -0,0 +1,69 @@
+package com.Draymond.NIO;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+public class TestBlockingNIO2 {
+    //客户端
+    @Test
+    public void client() throws IOException {
+        SocketChannel socketChannel=SocketChannel.open(new InetSocketAddress("127.0.0.1",9898));
+
+        FileChannel inChannel=FileChannel.open(Paths.get("1.png"), StandardOpenOption.READ);
+
+        ByteBuffer byteBuffer=ByteBuffer.allocate(1024);
+
+        while(inChannel.read(byteBuffer)!= -1){
+            byteBuffer.flip();
+            socketChannel.write(byteBuffer);
+            byteBuffer.clear();
+
+        }
+
+        socketChannel.shutdownOutput();
+
+        inChannel.close();
+        socketChannel.close();
+
+    }
+
+    //服务端
+    @Test
+    public void server() throws IOException{
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        
+        FileChannel outChannel= FileChannel.open(Paths.get("2.png"), StandardOpenOption.WRITE, StandardOpenOption.CREATE);
+
+        serverSocketChannel.bind(new InetSocketAddress(9898));
+
+        ByteBuffer buf = ByteBuffer.allocate(1024);
+
+        SocketChannel socketChannel1 = serverSocketChannel.accept();
+
+        while(socketChannel1.read(buf)!=-1){
+            buf.flip();
+            outChannel.write(buf);
+            buf.clear();
+        }
+
+        //发送反馈给客户端
+        buf.put("服务端接收数据成功".getBytes());
+        buf.flip();
+        socketChannel1.write(buf);
+
+        socketChannel1.close();
+        outChannel.close();
+        serverSocketChannel.close();
+    }
+
+}
